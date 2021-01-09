7d23f72870e49b6ff13405ee503390540f6d8be1
Jean Schoenheit <jean@outwiththebreeze.com>
2021-01-08 20:19:45 -0500

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World");
+      
+      //name is a reference variable
+      String name = "George Washington";
+      
+      System.out.println(name);
+      
+      //age is a primitive variable
+      int age = 24;
+      
+      System.out.println(age);
+      
+   }//end main
+}//end class
