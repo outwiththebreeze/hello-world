66d2855fb3adb2a6e8499212a7563a8452878714
Jean Schoenheit <jean@outwiththebreeze.com>
2021-01-11 14:55:19 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,15 +4,7 @@ public class HelloWorld
    {
       System.out.println("Hello World");
       
-      //name is a reference variable
-      String name = "George Washington";
-      
-      System.out.println(name);
-      
-      //age is a primitive variable
-      int age = 24;
-      
-      System.out.println(age);
+ 
       
    }//end main
 }//end class
